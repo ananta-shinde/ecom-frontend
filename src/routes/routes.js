4f167d7a1d1f511e8aa4d347e442a6a39ec2771d
Rohit Garad <rohitgarad96k@gmail.com>
2026-04-16 11:26:39 +0530

    16/04/2026 Rohit product listing component added

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,11 +1,24 @@
 import { createBrowserRouter } from "react-router-dom";
 import AdminDashboard from "../admin/AdminDashboard";
+import Home from "../customer/Home";
+import ProductListing from "../customer/ProductListing";
+import BaseTemplate from "../templates/BaseTemplate";
 import AddNewProductForm from "../admin/components/AddNewProductForm";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    element: <div>Hello World</div>,
+    path:"/",
+    element:<BaseTemplate />,
+    children:[
+      {
+        path: "",
+        element: <Home />
+      },
+      {
+        path: "products",
+        element: <ProductListing />
+      }
+    ]
   },
   {
     path:"/admin/dashboard",
